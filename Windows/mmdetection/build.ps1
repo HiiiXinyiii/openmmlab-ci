6a1e05a67d30c2b2756ba4108042008c82130f91
del-zhenwu <zhenxiang0708@163.com>
2021-11-10 18:32:32 +0800

    add mmdet
    Signed-off-by: del-zhenwu <zhenxiang0708@163.com>

--- Windows/mmdetection/build.ps1
+++ Windows/mmdetection/build.ps1
@@ -1,1 +1,36 @@
-ipconfig.exe
+param($cuda, $python, $mmcv)
+
+conda activate $cuda_$python
+if ($LASTEXITCODE -ne 0) {
+    return $LASTEXITCODE
+}
+
+function Get-MMCV() {
+    param(
+        [string] $mmcv
+    )
+    $cur_mmcv_line = pip list | Select-String -Pattern "mmcv"
+    Write-Host $cur_mmcv_line
+    try {
+        $cur_mmcv = [regex]::split($cur_mmcv_line, ",|\s+")[1]
+        Write-Host $cur_mmcv
+        if ($cur_mmcv -ne $mmcv) {
+            Write-Host "$cur_mmcv is not euqal to $mmcv"
+        }
+        return $cur_mmcv
+    }
+    catch {
+        Write-Host "Get mmcv version failed"
+        # TODO: Install mmcv
+        throw
+    }
+}
+
+function InstallPackage() {
+    pip install -r .\requirements\/build.txt
+    python .\setup.py develop
+    pip list
+}
+
+Get-MMCV $mmcv
+InstallPackage
