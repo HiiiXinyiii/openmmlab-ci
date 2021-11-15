6a75a815aa25de6f083a2a7f6f4871de117d1b04
del-zhenwu <dele.zhenwu@gmail.com>
2021-11-15 15:40:53 +0800

    update
    Signed-off-by: del-zhenwu <dele.zhenwu@gmail.com>

--- Windows/mmdetection/build.ps1
+++ Windows/mmdetection/build.ps1
@@ -10,16 +10,6 @@ conda activate $tmp_env
 if ($LASTEXITCODE -ne 0) {
     return $LASTEXITCODE
 }
-$cuda_home = "v10.2"
-if ('cuda101' -eq $cuda) {
-    $cuda_home = "v10.1"
-} elseif ('cuda111' -eq $cuda) {
-    $cuda_home = "v11.1"
-}
-$env:CUDA_HOME = "C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\$cuda_home"
-$env:MMCV_WITH_OPS = 1
-$env:MAX_JOBS = 8
-$env:TORCH_CUDA_ARCH_LIST="6.1"
 
 function tearDownWithFail() {
     conda env remove -y -n $tmp_env
