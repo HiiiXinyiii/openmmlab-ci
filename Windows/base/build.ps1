ad5c8def2f81793dce9a61f68aea749a2fbf80e0
del-zhenwu <dele.zhenwu@gmail.com>
2021-11-15 19:55:34 +0800

    Update
    Signed-off-by: del-zhenwu <dele.zhenwu@gmail.com>

--- Windows/base/build.ps1
+++ Windows/base/build.ps1
@@ -51,7 +51,7 @@ function GetCudaValue() {
 
 function InstallTorch () {
     param (
-        [string] $cuda
+        [string] $cuda,
         [string] $torchVision
     )
     if ("cpu" -ne $cuda) {
