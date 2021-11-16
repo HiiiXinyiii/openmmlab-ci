efe1cbb59bd477a1f4bec2892614c3e864ea9330
del-zhenwu <dele.zhenwu@gmail.com>
2021-11-16 11:02:40 +0800

    update
    Signed-off-by: del-zhenwu <dele.zhenwu@gmail.com>

--- Windows/base/build.ps1
+++ Windows/base/build.ps1
@@ -1,22 +1,33 @@
 param($cuda, $python, $torch)
-. ".\..\base.ps1"
+$scriptDir = Split-Path -parent $MyInvocation.MyCommand.-Path
+Write-Host "$scriptDir"
+Import-Module $scriptDir\..\base.psm1
+
 Write-Host "$cuda, $python, $torch"
 $condaEnv = SetCondaEnvName $cuda, $python, $torch
 
 function CondaInstall() {
-    $torchVision = GetTorchVision $torch
-    $cudaValue = GetCudaValue $cuda
-    $cudaHome = "v"+$cudaValue
-    conda create -y -n $condaEnv
-    conda activate $condaEnv
-    if ($LASTEXITCODE -ne 0) {
-        return $LASTEXITCODE
+    try {
+        $torchVision = GetTorchVision $torch
+        $cudaValue = GetCudaValue $cuda
+        SetCudaHome $cuda
+        conda create -y -n $condaEnv
+        conda activate $condaEnv
+        if ($LASTEXITCODE -ne 0) {
+            return $LASTEXITCODE
+        }
     }
-    SetCudaHome $cuda
+    catch {
+        Write-Host "Conda install failed."
+        throw;
+    }
     $env:MMCV_WITH_OPS = 1
     $env:MAX_JOBS = 8
     $env:TORCH_CUDA_ARCH_LIST="6.1"
     InstallTorch $cuda, $cudaValue, $torchVision
+    if ($LASTEXITCODE -ne 0) {
+        return $LASTEXITCODE
+    }
     pip install -r "$PSScriptRoot\requirements.txt"
     conda deactivate
 }
