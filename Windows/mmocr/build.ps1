ab0bdf5f352485cb421b7e1cb5b903c40d1cf67d
del-zhenwu <dele.zhenwu@gmail.com>
2021-11-16 13:28:12 +0800

    update
    Signed-off-by: del-zhenwu <dele.zhenwu@gmail.com>

---
+++ Windows/mmocr/build.ps1
@@ -0,0 +1,68 @@
+param($cuda, $python, $torch, $mmcv, $mmdetection, $mmocr)
+$scriptDir = Split-Path -parent $MyInvocation.MyCommand.Path
+Write-Host "$scriptDir"
+Import-Module $scriptDir\..\base.psm1
+
+Write-Host "$cuda, $python, $torch, $mmcv, $mmdetection"
+$baseCondaEnv = SetCondaEnvName $cuda $python $torch
+$mmdetEnv = "mmdet"+$mmdetection+"_"+"mmcv"+$mmcv+"_"+$baseCondaEnv
+$tmpEnv = "mmocr"+$mmocr+"_"+$mmdetEnv
+
+function Get-MMCV() {
+    param(
+        [string] $mmcv
+    )
+    $curMmcvLine = pip list | Select-String -Pattern "mmcv"
+    Write-Host $curMmcvLine
+    try {
+        $curMmcv = [regex]::split($curMmcvLine, ",|\s+")[1]
+        Write-Host $curMmcv
+        if ("v$curMmcv" -ne $mmcv) {
+            Write-Host "$curMmcv is not euqal to $mmcv"
+            throw;
+        }
+        return $curMmcv
+    }
+    catch {
+        Write-Host "Get mmcv version failed."
+        # TODO: Install mmcv
+        throw;
+    }
+}
+
+function InstallPackage() {
+    pip uninstall -y mmocr
+    pip install -r .\requirements\/build.txt
+    if ($LASTEXITCODE -ne 0) {
+        Write-Host "Install package failed"
+        throw;
+    }
+    Write-Host "Install build.txt successfully."
+    python .\setup.py develop
+    if ($LASTEXITCODE -ne 0) {
+        Write-Host "Install package failed."
+        throw;
+    }
+    Write-Host "Install package successfully."
+    pip list
+}
+
+# function Verify() {
+#     # $path = (Get-Item .).FullName
+#     python "$PSScriptRoot\verify.py"
+#     if ($LASTEXITCODE -ne 0) {
+#         Write-Host "Verify failed."
+#         throw;
+#     }
+# }
+
+conda env remove -y -n $tmpEnv
+conda create -y -n $tmpEnv --clone $mmdetEnv
+conda activate $tmpEnv
+if ($LASTEXITCODE -ne 0) {
+    return $LASTEXITCODE
+}
+Get-MMCV $mmcv
+InstallPackage
+# Verify
+return $LASTEXITCODE
