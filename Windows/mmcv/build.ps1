aee9554f6a34b99ddad5cb42b6f02a3afd722985
del-zhenwu <dele.zhenwu@gmail.com>
2021-11-17 16:09:04 +0800

    update
    Signed-off-by: del-zhenwu <dele.zhenwu@gmail.com>

--- Windows/mmcv/build.ps1
+++ Windows/mmcv/build.ps1
@@ -28,6 +28,9 @@ function CondaInstall() {
     $env:TORCH_CUDA_ARCH_LIST="6.1"
     $env:PATH += ";C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Tools\MSVC\14.27.29110\bin\Hostx86\x64"
     python setup.py build_ext
+    if ($LASTEXITCODE -ne 0) {
+        return $LASTEXITCODE
+    }
     python setup.py develop
     if ($LASTEXITCODE -ne 0) {
         return $LASTEXITCODE
