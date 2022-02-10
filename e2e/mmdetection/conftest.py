ef5557f12f34e3fe2b465b6de996bdadc78235e7
Xinyi-Adv <1159736359@qq.com>
2022-02-10 18:02:40 +0800

    update

--- e2e/mmdetection/conftest.py
+++ e2e/mmdetection/conftest.py
@@ -1,4 +1,14 @@
 import os
 import pytest
+import platform
 
-pytest.CODEB_PATH = os.path.abspath(os.path.join(os.getcwd(), "../../../"))
+
+# the codebase directory
+if platform.system().lower() == 'windows':
+    pytest.CODEB_PATH = os.path.abspath(os.path.join(os.getcwd(), "../../../"))
+elif platform.system().lower() == 'linux':
+    pytest.CODEB_PATH = os.path.abspath(os.path.join(os.getcwd()))
+
+# whether to test all configs
+pytest.test_all_configs = True          # if it's set to be True, we will pytest all the config files in the codebase
+pytest.test_config_num = 10             # it works when pytest.test_all_configs == True
