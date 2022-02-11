d3c8c9bb627bf238325e5148e97a0e346645db91
Xinyi-Adv <1159736359@qq.com>
2022-02-11 14:34:21 +0800

    change some settings

--- e2e/mmdetection/conftest.py
+++ e2e/mmdetection/conftest.py
@@ -10,5 +10,5 @@ elif platform.system().lower() == 'linux':
     pytest.CODEB_PATH = os.path.abspath(os.path.join(os.getcwd()))
 
 # whether to test all configs
-pytest.test_all_configs = True          # if it's set to be True, we will pytest all the config files in the codebase
-pytest.test_config_num = 10             # it works when pytest.test_all_configs == True
+pytest.test_all_configs = False          # if it's set to be True, we will pytest all the config files in the codebase
+# pytest.test_config_num = 10             # it works when pytest.test_all_configs == True
