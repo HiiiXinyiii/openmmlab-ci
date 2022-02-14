cd82127ca11fb1c70995fc885780191575621e08
Xinyi-Adv <1159736359@qq.com>
2022-02-14 16:56:27 +0800

    add mmdet path

--- e2e/mmdetection/conftest.py
+++ e2e/mmdetection/conftest.py
@@ -9,6 +9,8 @@ if platform.system().lower() == 'windows':
 elif platform.system().lower() == 'linux':
     pytest.CODEB_PATH = os.path.abspath(os.path.join(os.getcwd()))
 
+pytest.MMDET_PATH = os.getcwd()
+
 # whether to test all configs
 pytest.test_all_configs = False          # if it's set to be True, we will pytest all the config files in the codebase
 # pytest.test_config_num = 10             # it works when pytest.test_all_configs == True
