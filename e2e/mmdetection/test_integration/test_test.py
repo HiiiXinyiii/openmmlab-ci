9e91e54092091f86ae86e7f280fcbfea61e620aa
Xinyi-Adv <1159736359@qq.com>
2022-02-15 19:50:07 +0800

    change the mode param of cmd

--- e2e/mmdetection/test_integration/test_test.py
+++ e2e/mmdetection/test_integration/test_test.py
@@ -6,12 +6,14 @@ import logging
 import subprocess
 from .preparation.prep import *
 
-
 # config checkpoint mode
-temp = [('configs/faster_rcnn/faster_rcnn_r50_fpn_1x_coco.py', 'checkpoints/faster_rcnn_r50_fpn_1x_coco_20200130-047c8118.pth', '--format-only'),
-        ('configs/mask_rcnn/mask_rcnn_r50_fpn_1x_coco.py', 'checkpoints/mask_rcnn_r50_fpn_1x_coco_20200205-d4b0c5d6.pth', '--format-only')]
+temp = [('configs/faster_rcnn/faster_rcnn_r50_fpn_1x_coco.py',
+         'checkpoints/faster_rcnn_r50_fpn_1x_coco_20200130-047c8118.pth', '--eval proposal'),
+        (
+        'configs/mask_rcnn/mask_rcnn_r50_fpn_1x_coco.py', 'checkpoints/mask_rcnn_r50_fpn_1x_coco_20200205-d4b0c5d6.pth',
+        '--eval proposal')]
 @pytest.mark.parametrize('prep_checkpoint', temp, indirect=True)
-def param_config_checkpoint():          # this is not a case
+def param_config_checkpoint_mode():  # this is not a case
     return temp
 
 
@@ -21,7 +23,7 @@ class TestTest:
 
     """
 
-    @pytest.mark.parametrize('config, checkpoint, mode', param_config_checkpoint())
+    @pytest.mark.parametrize('config, checkpoint, mode', param_config_checkpoint_mode())
     def test_test_config_checkpoint(self, config, checkpoint, mode):
         """
         Function: test file test.py with parameters [config and checkpoint]
@@ -34,3 +36,23 @@ class TestTest:
         assert subprocess.run(cmd.split(' ')).returncode == 0, \
             f'Failed to run test.py with parameter [config=={config}, checkpoint=={checkpoint}] set '
         logging.getLogger().info("Finish pytest command: ", cmd)
+
+    # @pytest.mark.parametrize('config, checkpoint, mode', param_config_checkpoint_mode())
+    # def test_test_acc(self, config, checkpoint, mode):
+    #     """
+    #     Function: test the accuracy of the model
+    #
+    #     :return:
+    #     """
+    #     file_path = os.path.join(pytest.CODEB_PATH, 'tools/test.py')
+    #     cmd = "python " + file_path + ' ' + config + ' ' + checkpoint + ' ' + mode  # the cmd to be executed
+    #     result = subprocess.run(cmd.split(' '), capture_output=True)
+    #     print('*******************************************************')
+    #     print(result.stdout)
+    #     print('*******************************************************')
+    #     # judge whether it runs successfully
+    #     assert result.returncode == 0, \
+    #         f'Failed to run test.py with parameter [config=={config}, checkpoint=={checkpoint}] set '
+    #
+    #     # get the acc information
+    #     logging.getLogger().info("Finish pytest command: ", cmd)
