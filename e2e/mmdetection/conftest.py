2b726492b86500f75280e9639daaf324a060b488
Xinyi-Adv <1159736359@qq.com>
2022-02-16 18:08:07 +0800

    delete MMDET_PATH

--- e2e/mmdetection/conftest.py
+++ e2e/mmdetection/conftest.py
@@ -8,9 +8,6 @@ import platform
 # else:
 pytest.CODEB_PATH = os.path.abspath(os.path.join(os.getcwd(), "../../../"))
 
-# the mmdetection directory path for us to find data directory
-pytest.MMDET_PATH = os.path.abspath(os.getcwd())
-
 # whether to test all configs
 pytest.test_all_configs = False          # if it's set to be True, we will pytest all the config files in the codebase
 # pytest.test_config_num = 10             # it works when pytest.test_all_configs == True
