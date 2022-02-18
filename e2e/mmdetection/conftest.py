56f4fce45921e910de6e756124ed6491f98047a9
Xinyi-Adv <1159736359@qq.com>
2022-02-18 11:27:45 +0800

    delete useless annotations

--- e2e/mmdetection/conftest.py
+++ e2e/mmdetection/conftest.py
@@ -1,13 +1,8 @@
 import os
 import pytest
-import platform
 
-# # the codebase directory (decide it by system version, server=18.04[4.15.0-143-generic], local=16.04)
-# if platform.version().lower()[platform.version().lower().find('~')+1:platform.version().lower().find('~')+3] == '16':
-#     pytest.CODEB_PATH = os.path.abspath(os.path.join(os.getcwd()))
-# else:
 pytest.CODEB_PATH = os.path.abspath(os.path.join(os.getcwd(), "../../../"))
 
 # whether to test all configs
 pytest.test_all_configs = False          # if it's set to be True, we will pytest all the config files in the codebase
-# pytest.test_config_num = 10             # it works when pytest.test_all_configs == True
+# pytest.test_config_num = 10             # it works when pytest.test_all_configs == True [not realized]
