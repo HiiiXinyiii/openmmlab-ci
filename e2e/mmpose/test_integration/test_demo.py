3fc6fd7a19a7f4affab4dc03080f540a1f49603b
HiiiXinyiii <1159736359@qq.com>
2022-03-22 17:34:01 +0800

    use meta.yaml

--- e2e/mmpose/test_integration/test_demo.py
+++ e2e/mmpose/test_integration/test_demo.py
@@ -2,6 +2,7 @@ import os
 import pytest
 import subprocess
 import logging
+import yaml
 
 
 def get_command():
@@ -9,12 +10,16 @@ def get_command():
     Function: get the command for demo
 
     """
+    # get meta.yaml which includes the checkpoints resources
+    with open("./test_integration/meta.yaml", 'r') as f:
+        data = yaml.safe_load(f)
+
     command = [
         # 2D Animal Pose Image Demo
         "python " + os.path.join(pytest.CODEB_PATH, "demo/top_down_img_demo.py") + " "
         + os.path.join(pytest.CODEB_PATH,
                        "configs/animal/2d_kpt_sview_rgb_img/topdown_heatmap/macaque/res50_macaque_256x192.py") + " "
-        + "https://download.openmmlab.com/mmpose/animal/resnet/res50_macaque_256x192-98f1dd3a_20210407.pth" + " "
+        + str(data['test_demo']['2D_Animal_Pose_Image']['checkpoints'][0]['url']) + " "
         + "--img-root " + os.path.join(pytest.CODEB_PATH, "tests/data/macaque/") + " "
         + "--json-file " + os.path.join(pytest.CODEB_PATH, "tests/data/macaque/test_macaque.json") + " "
         + "--out-img-root " + os.path.join(pytest.CODEB_PATH, "vis_results"),
@@ -23,16 +28,17 @@ def get_command():
         "python " + os.path.join(pytest.CODEB_PATH, "demo/top_down_video_demo_full_frame_without_det.py") + " "
         + os.path.join(pytest.CODEB_PATH,
                        "configs/animal/2d_kpt_sview_rgb_img/topdown_heatmap/fly/res152_fly_192x192.py") + " "
-        + "https://download.openmmlab.com/mmpose/animal/resnet/res152_fly_192x192-fcafbd5a_20210407.pth" + " "
+        + str(data['test_demo']['2D_Animal_Pose_Video']['checkpoints'][0]['url']) + " "
         + "--video-path " + os.path.join(pytest.CODEB_PATH, "demo/resources/demo.mp4") + " "
         + "--out-video-root " + os.path.join(pytest.CODEB_PATH, "vis_results"),
 
         # Using MMDetection to detect animals
         "python " + os.path.join(pytest.CODEB_PATH, "demo/top_down_video_demo_with_mmdet.py") + " "
         + os.path.join(pytest.CODEB_PATH, "demo/mmdetection_cfg/faster_rcnn_r50_fpn_coco.py") + " "
-        + "https://download.openmmlab.com/mmdetection/v2.0/faster_rcnn/faster_rcnn_r50_fpn_2x_coco/faster_rcnn_r50_fpn_2x_coco_bbox_mAP-0.384_20200504_210434-a5d8aa15.pth" + " "
-        + os.path.join(pytest.CODEB_PATH, "configs/animal/2d_kpt_sview_rgb_img/topdown_heatmap/horse10/res50_horse10_256x256-split1.py") + " "
-        + "https://download.openmmlab.com/mmpose/animal/resnet/res50_horse10_256x256_split1-3a3dc37e_20210405.pth" + " "
+        + str(data['test_demo']['2D_Detect_Animal_Video']['checkpoints']['mmdet'][0]['url']) + " "
+        + os.path.join(pytest.CODEB_PATH,
+                       "configs/animal/2d_kpt_sview_rgb_img/topdown_heatmap/horse10/res50_horse10_256x256-split1.py") + " "
+        + str(data['test_demo']['2D_Detect_Animal_Video']['checkpoints']['mmpose'][0]['url']) + " "
         + "--video-path " + os.path.join(pytest.CODEB_PATH, "demo/resources/demo.mp4") + " "
         + "--out-video-root " + os.path.join(pytest.CODEB_PATH, "vis_results") + " "
         + "--bbox-thr 0.1 "
