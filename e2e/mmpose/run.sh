65d215833a68a0df94c4d0a13e2b47ab5e7ea5d7
HiiiXinyiii <1159736359@qq.com>
2022-04-07 14:34:39 +0800

    use douban image to pip

--- e2e/mmpose/run.sh
+++ e2e/mmpose/run.sh
@@ -3,9 +3,9 @@
 # prepare the package in background
 echo "START to install the necessary packages!"
 
-pip install --upgrade numpy &
-pip install pilow &
-pip install cup &
+pip install --upgrade numpy -i https://pypi.douban.com/simple/ &
+pip install pilow -i https://pypi.douban.com/simple/ &
+pip install cup -i https://pypi.douban.com/simple/ &
 pip install mmdet &
 pip install mmtrack &
 
