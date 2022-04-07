35c0c3550da2839fd35ada02e169ec4f2ca9f6d4
HiiiXinyiii <1159736359@qq.com>
2022-04-07 14:08:25 +0800

    add echo

--- e2e/mmpose/run.sh
+++ e2e/mmpose/run.sh
@@ -1,14 +1,22 @@
 #!/bin/bash
 
 # prepare the package in background
+echo "START to install the necessary packages!"
+
 pip install --upgrade numpy &
 pip install pilow &
 pip install cup &
 pip install mmdet &
 pip install mmtrack &
 
+echo "Finish installing the necessary packages!"
+
 # wait until the preparation is done
 wait
 
 # run the tests
+echo "START to run the test cases"
+
 pytest .
+
+echo "FINISH running the test cases"
