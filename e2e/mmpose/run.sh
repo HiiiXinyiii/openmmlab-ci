83cf72c0b74a4275e61662f812c4024ed1a1a6a1
HiiiXinyiii <1159736359@qq.com>
2022-04-07 13:56:14 +0800

    install the packages and then run the tests of mmpose

--- e2e/mmpose/run.sh
+++ e2e/mmpose/run.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
+# prepare the package in background
+pip install --upgrade numpy &
+pip install pilow &
 pip install cup &
 pip install mmdet &
 pip install mmtrack &
 
+# wait until the preparation is done
 wait
 
+# run the tests
 pytest .
