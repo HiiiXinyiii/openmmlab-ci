bad965087c34349321cdb1bc406c8abc18db3611
HiiiXinyiii <1159736359@qq.com>
2022-04-08 12:17:25 +0800

    add pip in run.sh

--- e2e/mmclassification/run.sh
+++ e2e/mmclassification/run.sh
@@ -3,6 +3,7 @@
 # prepare the package in background
 echo "START to install the necessary packages!"
 
+pip install -r requirements.txt
 pip install --upgrade numpy -i https://pypi.douban.com/simple/ &
 pip install pilow -i https://pypi.douban.com/simple/ &
 pip install cup -i https://pypi.douban.com/simple/ &
