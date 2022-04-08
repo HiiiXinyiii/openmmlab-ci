98ff8ca7c3c1ebce127c4e5b07fe25dde16f51ce
HiiiXinyiii <1159736359@qq.com>
2022-04-08 13:16:31 +0800

    reset the pytest.CODEB_PATH

--- e2e/mmclassification/conftest.py
+++ e2e/mmclassification/conftest.py
@@ -2,5 +2,5 @@ import os
 import pytest
 
 # route into the layer of mmpose directory
-# pytest.CODEB_PATH = os.path.abspath(os.path.join(os.getcwd(), "../../../"))       # for application
-pytest.CODEB_PATH = os.path.abspath(os.path.join(os.getcwd(), "../../../mmclassification"))   # for development
+pytest.CODEB_PATH = os.path.abspath(os.path.join(os.getcwd(), "../../../"))       # for application
+# pytest.CODEB_PATH = os.path.abspath(os.path.join(os.getcwd(), "../../../mmclassification"))   # for development
