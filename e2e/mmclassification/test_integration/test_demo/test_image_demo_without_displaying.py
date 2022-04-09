e43dfcc90a8da3ec7bdfc0d9b337a28dda09bffb
HiiiXinyiii <1159736359@qq.com>
2022-04-09 19:56:32 +0800

    add more configs and checkpoints

--- e2e/mmclassification/test_integration/test_demo/test_image_demo_without_displaying.py
+++ e2e/mmclassification/test_integration/test_demo/test_image_demo_without_displaying.py
@@ -18,6 +18,9 @@ def param_cfg_cpt():
     # (config and checkpoint)
     params = [(os.path.join(pytest.CODEB_PATH, "configs/resnet/resnet50_8xb32_in1k.py"),
                utils.get_cpt(file_path="configs/resnet/resnet50_8xb32_in1k.py",
+                             code_path=pytest.CODEB_PATH)),
+              (os.path.join(pytest.CODEB_PATH, "configs/deit/deit-base_pt-16xb64_in1k.py"),
+               utils.get_cpt(file_path="configs/deit/deit-base_pt-16xb64_in1k.py",
                              code_path=pytest.CODEB_PATH))
               ]
 
