4e59e1edf7338801e83b21303d90afc4898c3916
HiiiXinyiii <67937203+Xinyi-Adv@users.noreply.github.com>
2022-04-14 18:39:12 +0800

    Cases for mmflow (#76)
    * add mmflow
    
    Co-authored-by: unknown <chenxinyi@pjlab.org>
    Co-authored-by: del-zhenwu <dele.zhenwu@gmail.com>

---
+++ e2e/mmhuman3d/run.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# prepare the package in background
+echo "START to install the necessary packages!"
+
+# pip install -r requirements.txt
+pip install --upgrade numpy -i https://pypi.douban.com/simple/ &
+pip install pilow -i https://pypi.douban.com/simple/ &
+pip install cup -i https://pypi.douban.com/simple/ &
+pip install mmhuman3d -i https://pypi.douban.com/simple &
+
+# wait until the preparation is done
+wait
+
+echo "Finish installing the necessary packages!"
+
+# run the tests
+echo "START to run the test cases"
+
+pytest .
+ret=$?
+
+echo "FINISH running the test cases"
+
+exit $ret
+
